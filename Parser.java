40ddafd53f38fbf0be26b1ac37791a22d522882e
Saheed Adepoju <adepoju@gmail.com>
2020-04-15 22:42:23 -0700

    Added some helper methods to parse variables and the most basic unit of the program. Compilation passes

--- Parser.java
+++ Parser.java
@@ -1,14 +1,14 @@
 /**
 * A class that parses the grammar of While programming language
 *
-*
+*Class will break input string into tokens
 *
 *
 */
 
 
 
-public Class Parser{
+public class Parser{
 
   private String expression;
   int position;
@@ -20,7 +20,7 @@ public Class Parser{
 
   // Handles white spaces within the expression.
   void consumeWhiteSpace(){
-    while(position < expression.length() && Character.isWhilespace(expression.charAt(position))){
+    while(position < expression.length() && Character.isWhitespace(expression.charAt(position))){
       position = position+1;
     }
 
@@ -30,13 +30,13 @@ public Class Parser{
   void consumeToken(String token){
       consumeWhiteSpace();
       if(position + token.length() <= expression.length() && expression.substring(position,position + token.length()).equals(token)){
-          position = position.token.length();
+          position = position + token.length();
 
       }
 
   }
 
-  boolean checkahead(string token){
+  boolean checkahead(String token){
       int start = position;
       boolean success;
       consumeToken(token);
@@ -46,7 +46,53 @@ public Class Parser{
 
   }
 
-  
+  enum Operator{PLUS,MINUS,MULT,SUB}
+
+/**
+* Tracks expressions with operators
+*
+*/
+  static class OperatorExpression{
+    private Operator ops;
+    Expression expression;
+
+    public OperatorExpression(Operator ops,Expression expression){
+      this.ops = ops;
+      this.expression = expression;
+    }
+
+
+
+
+  }
+
+//A concrete class which keeps a data structure that reflects the structure of the expression that has been tokenized.
+//This consumes the smallest unit of the expression.
+  Expression unit(){
+    int start = position;
+    Expression result;
+    result = variable();
+
+    return result;
+
+
+  }
+
+  Variable variable(){
+      consumeWhiteSpace();
+      int start = position;
+      while(position < expression.length() && (expression.charAt(position) >='a' && expression.charAt(position) <='z')){
+        position++;
+      }
+      if(position > start){
+        return new Variable(expression.substring(start,position));
+      }
+      return null;
+  }
+
+
+
+
 
 
 
